13eb25fb3c3bf3267cb5c6b02ed4abeb1550685a
Daniel Tschumi <daniel.tschumi@gmx.com>
2018-01-27 17:10:03 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'bootstrap-css','font-awesome-css','slicknav-css' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+         
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'chld_thm_cfg_parent','kotha-stylesheet','kotha-responsive' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css' );
+
+// END ENQUEUE PARENT ACTION
+
+// Load translation files from the child theme instead of the parent theme
+function my_child_theme_locale() {
+    load_child_theme_textdomain( 'kotha', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'my_child_theme_locale' );
+
+// Special files doesn't get included automatically
+require_once get_stylesheet_directory() . '/inc/template-tags.php';
